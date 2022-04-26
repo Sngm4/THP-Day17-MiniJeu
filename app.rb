ab91141bbc8363e0a825d375fc0cefb7a066e3a2
Iris <marlx42@protonmail.com>
2022-04-26 21:50:10 +0200

    Les problèmeeeees

--- app.rb
+++ app.rb
@@ -6,6 +6,7 @@ require_relative 'lib/player'
 
 player1 = Player.new("Iris La Sanguinaire", 10)
 player2 = Player.new("Matthias Le Mercenaire", 10)
+humanplayer1 = HumanPlayer.new("La jolie Mary", 100, 1)
 
 puts "*"*100
 puts ""
@@ -38,3 +39,5 @@ end
 puts "*"*100
 puts ""
 puts "Ce fût un combat sans merci, nous remercions #{player1.name} et #{player2.name} pour leur participations, c'était MOR-TEL !"
+
+binding.pry
--- lib/player.rb
+++ lib/player.rb
@@ -31,3 +31,49 @@ class Player
   end
 
 end
+
+class HumanPlayer < Player
+  attr_accessor :weapon_level
+
+  def initialize(name, life_points, weapon_level)
+    @weapon_level = weapon_level
+    super(name, life_points)
+  end
+
+  def show_state
+    puts "#{@name} a #{@life_points} points de vie et en plus une arme de niveau #{weapon_level} !"
+  end
+  
+  def search_weapon
+    rand(1..6) = new_weapon
+    puts "GG ! Tu viens de trouver une nouvelle arme de niveau #{new_weapon}"
+    if new_weapon > weapon_level do
+      weapon_level = new_weapon
+      puts "Cette arme est meilleur que la tienne, tu l'as prends !"
+    else 
+      new_weapon <= weapon_level do 
+        puts "DAMN, cette arme vaut R. Autant garder celle que tu as déjà..."
+      end
+  end
+
+  def search_health_pack
+    rand(1..6) = pack
+    if pack == 1
+      puts "Tu n'as rien trouvé..."
+    elsif pack == 2..5 do 
+       if life_points <=50  do
+        life_points = life_points + 50
+        puts "GG BG ! Tu viens de trouver un pack de +50 points de vie"
+       else life_points > 100 do
+        puts "Tu as trouvé 50 points de vie, mais tu as déjà 100 points !"
+       end
+    elsif pack == 6 do
+      if life_points < 100 &&  
+      puts 
+
+
+  def compute_damage
+    rand(1..6) * @weapon_level
+  end
+end
+
